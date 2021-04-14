732d8b3c8661cec152b4dae00b9523eebc25f2d2
rz67749 <rz67749@uga.edu>
2021-04-14 11:07:04 -0400

    Changed the look of the "MainActivity" page to make it look cleaner. Seems like the file is getting too big and may cause a crash. Have to keep that in mind. May need to resize images that I was using.

--- app/src/main/java/com/example/electrochromicfilm/MainActivity.java
+++ app/src/main/java/com/example/electrochromicfilm/MainActivity.java
@@ -23,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
         setContentView(R.layout.activity_main);
 
         // Transfers data to this activity, but maybe we can still see this information when we go
