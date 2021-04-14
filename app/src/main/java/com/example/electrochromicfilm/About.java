82719bb5cc795fa5f17f73fe70122f1411ea7eea
rz67749 <rz67749@uga.edu>
2021-04-14 07:43:29 -0400

    Fixed the "About" page

--- app/src/main/java/com/example/electrochromicfilm/About.java
+++ app/src/main/java/com/example/electrochromicfilm/About.java
@@ -3,6 +3,7 @@ package com.example.electrochromicfilm;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +16,9 @@ public class About extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
         setContentView(R.layout.aboutactivity);
         initListeners();
     }
